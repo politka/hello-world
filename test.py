a26aa7f3ea2cf6b4731c88ba17a8c7ff39966f4e
Polina Korosteleva <politka@hotmail.co.uk>
2020-02-27 14:24:52 +0000

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ MONGO_HOST = "10.1.0.90"
 MONGO_PORT = 25
 MONGO_DB = "db_name"
 MONGO_USER = "username"
-MONGO_PASS = "password"
+MONGO_PASS = "qwerty1237"
 
 con = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
 db = con[MONGO_DB]
